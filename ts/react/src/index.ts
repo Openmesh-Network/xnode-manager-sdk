cf48572f13dc7654cb06197ec5be2e3f888d69db
Plopmenz <plopmenz@gmail.com>
2025-06-18 13:42:43 +0200

    react 1.0.5

--- ts/react/src/index.ts
+++ ts/react/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./auth.js";
 export * from "./config.js";
 export * from "./file.js";
+export * from "./info.js";
 export * from "./os.js";
 export * from "./process.js";
 export * from "./request.js";
