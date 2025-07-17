325998695f44c9c606de622a36baa0290b3cf20d
Plopmenz <plopmenz@gmail.com>
2025-07-17 16:23:05 +0200

    typescript 1.0.7

--- ts/package/src/usage/handlers.ts
+++ ts/package/src/usage/handlers.ts
@@ -14,7 +14,7 @@ export type cpu_input = Sessioned<{
 }>;
 export type cpu_output = rust.Vec<CpuUsage>;
 export async function cpu(input: cpu_input): Promise<cpu_output> {
-  return SessionGet(input, scope(), (path) => `/${path}/cpu`);
+  return SessionGet(input, scope(), (path) => `/${path.scope}/cpu`);
 }
 
 export type memory_input = Sessioned<{
@@ -24,7 +24,7 @@ export type memory_input = Sessioned<{
 }>;
 export type memory_output = MemoryUsage;
 export async function memory(input: memory_input): Promise<memory_output> {
-  return SessionGet(input, scope(), (path) => `/${path}/memory`);
+  return SessionGet(input, scope(), (path) => `/${path.scope}/memory`);
 }
 
 export type disk_input = Sessioned<{
@@ -37,6 +37,6 @@ export async function disk(input: disk_input): Promise<disk_output> {
   return SessionGet<disk_output, disk_input["path"]>(
     input,
     scope(),
-    (path) => `/${path}/disk`
+    (path) => `/${path.scope}/disk`
   ).then((data) => data.filter((d) => d.mount_point.startsWith("/mnt")));
 }
