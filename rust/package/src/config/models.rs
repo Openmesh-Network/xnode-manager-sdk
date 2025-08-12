8f059ab71dd3762fadc5ce948427059659bedf1d
Plopmenz <plopmenz@gmail.com>
2025-08-12 14:51:56 +0200

    rust 1.0.2

--- rust/package/src/config/models.rs
+++ rust/package/src/config/models.rs
@@ -5,12 +5,14 @@ pub struct ContainerConfiguration {
     pub flake: String,
     pub flake_lock: Option<String>,
     pub network: Option<String>,
+    pub nvidia_gpus: Option<Vec<u64>>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct ContainerSettings {
     pub flake: String,
     pub network: Option<String>,
+    pub nvidia_gpus: Option<Vec<u64>>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
